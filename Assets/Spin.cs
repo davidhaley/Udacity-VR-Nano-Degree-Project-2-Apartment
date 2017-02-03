775cd8fa30e42490a161120a3e620628a177bf6d
David Haley <djdavidhaley@gmail.com>
2017-02-03 09:58:25 -0700

    Add Unity assets and project settings to repo

---
+++ Assets/Spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    private bool spinning;
+
+	void Update () {
+		if (spinning)
+        {
+            transform.Rotate(Vector3.up * (Time.deltaTime * 200f));
+        }
+	}
+
+    public void SpinObject()
+    {
+        if (!spinning)
+        {
+            spinning = true;
+        }
+        else if (spinning)
+        {
+            spinning = false;
+        }
+    }
+}
